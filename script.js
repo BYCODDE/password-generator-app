508c499b2932738a34ae0ef0b747233dad0fc62d
BYCODDE <anaraliev217@gmail.com>
2024-04-07 19:01:39 +0400

    input problem solved!

--- script.js
+++ script.js
@@ -17,5 +17,11 @@ range.addEventListener("input", function () {
   zero.textContent = range.value;
 });
 
-
-
+range.addEventListener("input", function () {
+  let x = this.value;
+  let colorStop = (x / (this.max - this.min)) * 100 + "%";
+  let gradient = `linear-gradient(to right, var(--Neon-Green, #A4FFAF) 0%, var(--Neon-Green, #A4FFAF) ${colorStop}, var(--Very-Dark-Grey, #18171F)
+ ${colorStop}, var(--Very-Dark-Grey, #18171F)
+ 100%)`;
+  range.style.background = gradient;
+});
