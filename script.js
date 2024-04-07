39ac1b9cf84d66053d14746d30d1896df6a7017d
BYCODDE <anaraliev217@gmail.com>
2024-04-07 21:48:33 +0400

    working on js!

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const main_span = document.getElementById("main_span");
 const passwordInput = document.querySelector(".top_section input");
 const zero = document.getElementById("zero");
 const range = document.getElementById("myrange");
-console.log(range.value);
-
+const checkbox = document.querySelectorAll('input[type="checkbox"]');
+const strength_svg = document.getElementById("first");
+const strength_svg2 = document.getElementById("second");
+const strength_svg3 = document.getElementById("third");
+const strength_svg4= document.getElementById("fourth");
+console.log(checkbox,strength_svg2,strength_svg3,strength_svg4);
 svg_button.addEventListener("click", function () {
   if (passwordInput.value.length > 0) {
     main_span.style.display = "block";
@@ -24,4 +28,15 @@ range.addEventListener("input", function () {
  ${colorStop}, var(--Very-Dark-Grey, #18171F)
  100%)`;
   range.style.background = gradient;
+});
+
+checkbox.forEach((checkbox) => {
+  console.log(checkbox);
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      strength_svg.style.fill = "red";
+    } else {
+      strength_svg.style.fill = "none";
+    }
+  });
 });
